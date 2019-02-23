354b4aded2c9a07b2eec97f227be0913665e3152
UnsignedByte <edlam1889@gmail.com>
2019-02-22 23:01:11 -0800

    weat

--- mantle.user.js
+++ mantle.user.js
@@ -185,7 +185,7 @@
       const [uid, portfolioID, pageID, public_hash] = id.split('-');
       return fetchJSON(`users/${uid}/portfolios/${portfolioID}/items/${pageID}`,
           {'X-Csrf-Token': this.csrfToken, 'X-Public-Hash': public_hash})
-        .then(({metadata: {content}}) => {encrypt(uid, content, false);});
+        .then(({metadata: {content}}) => encrypt(uid, content, false));
     }
 
     getID() {
